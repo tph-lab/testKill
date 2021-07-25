98da4b99a5b2e62a2d19a9ae2fbbe8ca00fd43d9
tph81 <2378868011@qq.com>
2021-07-25 17:00:02 +0800

    first commit

---
+++ src/main/java/com/yc/testseckill/TestSecKillApplication.java
@@ -0,0 +1,13 @@
+package com.yc.testseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSecKillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yc/testseckill/TestSecKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.testseckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
